8474d18a798ced9fb638c9f5f4b212f6d861576f
Smith Zhang <smithzhang0427@gmail.com>
2021-12-27 16:26:41 -0500

    update style

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -30,9 +30,9 @@ export const SalesListView = () => {
   return (
     <>
       <Banner
-        src="/main-banner.svg"
-        headingText="The amazing world of Metaplex."
-        subHeadingText="Buy exclusive Metaplex NFTs."
+        src=""
+        headingText="The amazing world of ACKCS."
+        subHeadingText="Buy exclusive ACKCS NFTs."
         actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
         useBannerBg
       />
